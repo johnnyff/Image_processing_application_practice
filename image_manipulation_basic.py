efdb5a446ddaef0710c31cfc619335ed085090b7
Johnny <rkd2016@gmail.com>
2021-07-10 19:09:12 +0900

    rename

--- image_manipulation_basic.py
+++ image_manipulation_basic.py
@@ -1,23 +1,19 @@
 import cv2
-import tensorflow as tf
-import dlib
-
 
 img = cv2.imread('01.jpg')
-print(img)
-print(img.shape)
+overlay_img = cv2.imread('dices.png', cv2.IMREAD_UNCHANGED)
 
-cv2.rectangle(img, pt1=(259, 89), pt2=(380, 348),
-              color=(255, 0, 0), thickness=2)
-cv2.circle(img, center=(320, 220), radius=100, color=(0, 0, 255), thickness=3)
-cropped_img = img[89:384, 259:380]  # y axis followed by x axis
+overlay_img = cv2.resize(overlay_img, dsize=(150, 150))
+# 이미지 합성
+overlay_alpha = overlay_img[:, :, 3:] / 255.0
+background_alpha = 1.0 - overlay_alpha
+x1 = 100
+y1 = 100
+x2 = x1 + 150
+y2 = y1 + 150
 
-img_resized = cv2.resize(img, (512, 256))
+img[y1:y2, x1:x2] = overlay_alpha * overlay_img[:,
+                                                :, :3] + background_alpha * img[y1:y2, x1:x2]
 
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-cv2.imshow('gray', img_gray)
-cv2.imshow("resized", img_resized)
-cv2.imshow("image", img)
-cv2.imshow("crop", cropped_img)
+cv2.imshow('img', img)
 cv2.waitKey(0)
